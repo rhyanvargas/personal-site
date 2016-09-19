25717dda5c134d3e64de95fe29b40a6727e5d793
Rhyan Vargas <rhyanv1@gmail.com>
2016-09-19 18:07:29 -0400

    edit resume content

---
+++ app/js/main.js
@@ -0,0 +1,14 @@
+<!-- Font Script -->
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Source+Sans+Pro:400,400italic:latin', 'Source+Serif+Pro:400,700:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
